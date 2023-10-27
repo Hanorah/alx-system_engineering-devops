2e68a42a02303d8b08a2fd0bed969df5e277843a
OkosodoNorah <ksdnorah@gmail.com>
2023-10-27 01:09:45 -0700

    commit

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
-import json
-import requests
 
+"""
+Python script that export data in the JSON format.
+"""
+
+from requests import get
+import json
 
 if __name__ == "__main__":
-    userId = 0
-    jsonDict = {}
-    rTodo = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
-    for d in rTodo:
-        if d.get('userId') != userId:
-            userId = d.get('userId')
-        rUser = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                             .format(userId))
-        emplUname = rUser.json().get('username')
-        if userId not in jsonDict.keys():
-            jsonDict[userId] = []
-        if d.get('userId') == int(userId):
-            jsonDict[userId].append({
-                'username': emplUname,
-                'task': d.get('title'),
-                'completed': d.get('completed')
-            })
-    with open('todo_all_employees.json', 'w') as file:
-        json.dump(jsonDict, file)
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+
+    new_dict1 = {}
+
+    for j in data2:
+
+        row = []
+        for i in data:
+
+            new_dict2 = {}
+
+            if j['id'] == i['userId']:
+
+                new_dict2['username'] = j['username']
+                new_dict2['task'] = i['title']
+                new_dict2['completed'] = i['completed']
+                row.append(new_dict2)
+
+        new_dict1[j['id']] = row
+
+    with open("todo_all_employees.json",  "w") as f:
+
+        json_obj = json.dumps(new_dict1)
+        f.write(json_obj)
