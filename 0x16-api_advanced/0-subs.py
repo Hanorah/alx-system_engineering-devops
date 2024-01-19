e81a167e64e0f648415033d47a005bfaf14d052c
Norah <ksdnorah@gmail.com>
2024-01-19 06:24:53 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,30 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+    Return the total number of subscribers on a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 404:
         return 0
+
     results = response.json().get("data")
     return results.get("subscribers")
+
+
+if __name__ == "__main__":
+    subreddit_name = "programming"
+    subscribers_count = number_of_subscribers(subreddit_name)
+    print(subscribers_count)
